002c25053fffa76e1c9986522dc5be9ed934fb74
M <markpackham1@gmail.com>
2025-12-11 15:14:43 +0000

    Python Essentials For Dummies sample code

---
+++ DateTimeDifference.py
@@ -0,0 +1,6 @@
+import datetime as dt
+new_years_day = dt.date(2024, 1, 1)
+memorial_day = dt.date(2024, 5, 27)
+days_between = memorial_day - new_years_day
+print(days_between)
+print(type(days_between))
